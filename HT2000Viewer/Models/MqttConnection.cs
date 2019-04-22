ca86855c3fc17470355a94ecc814633d3f17ca8f
Alex <temper8@gmail.com>
2019-04-22 13:48:40 +0300

    add mqtt topic for humidity and Co2

--- HT2000Viewer/Models/MqttConnection.cs
+++ HT2000Viewer/Models/MqttConnection.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
+using static Windows.Storage.ApplicationData;
 
 namespace HT2000Viewer.Models
 {
@@ -24,8 +25,8 @@ namespace HT2000Viewer.Models
 
         public string username
         {
-            get => (string)Windows.Storage.ApplicationData.Current.LocalSettings.Values["username"];
-            set => Windows.Storage.ApplicationData.Current.LocalSettings.Values["username"] = value;
+            get => (string)Current.LocalSettings.Values["username"];
+            set => Current.LocalSettings.Values["username"] = value;
         }
 
         public string password
@@ -36,9 +37,48 @@ namespace HT2000Viewer.Models
 
         public string clientId
         {
-            get => (string)Windows.Storage.ApplicationData.Current.LocalSettings.Values["clientId"];
+            get {
+                if (!Current.LocalSettings.Values.ContainsKey("clientId"))
+                    Current.LocalSettings.Values["clientId"] = Guid.NewGuid().ToString();
+                return (string)Current.LocalSettings.Values["clientId"];
+            }
             set => Windows.Storage.ApplicationData.Current.LocalSettings.Values["clientId"] = value;
         }
+        
+
+        public string TemperatureTopic
+        {
+            get
+            {
+                if (!Current.LocalSettings.Values.ContainsKey("TemperatureTopic"))
+                    Current.LocalSettings.Values["TemperatureTopic"] = $"/home/temperature";
+                return (string)Current.LocalSettings.Values["TemperatureTopic"];
+            }
+            set => Windows.Storage.ApplicationData.Current.LocalSettings.Values["TemperatureTopic"] = value;
+        }
+
+        public string HumidityTopic
+        {
+            get
+            {
+                if (!Current.LocalSettings.Values.ContainsKey("HumidityTopic"))
+                    Current.LocalSettings.Values["HumidityTopic"] = $"/home/humidity";
+                return (string)Current.LocalSettings.Values["HumidityTopic"];
+            }
+            set => Windows.Storage.ApplicationData.Current.LocalSettings.Values["HumidityTopic"] = value;
+        }
+
+        public string CO2Topic
+        {
+            get
+            {
+                if (!Current.LocalSettings.Values.ContainsKey("CO2Topic"))
+                    Current.LocalSettings.Values["CO2Topic"] = $"/home/co2";
+                return (string)Current.LocalSettings.Values["CO2Topic"];
+            }
+            set => Windows.Storage.ApplicationData.Current.LocalSettings.Values["CO2Topic"] = value;
+        }
+
 
         MqttClient client = null;
 
@@ -48,7 +88,7 @@ namespace HT2000Viewer.Models
             {
                 client = new MqttClient(brokerHostName, brokerPort, false, MqttSslProtocols.None);
 
-                clientId = Guid.NewGuid().ToString();
+                //clientId = Guid.NewGuid().ToString();
                 client.Connect(clientId, username, password);
             }
             catch (Exception e)
@@ -61,7 +101,7 @@ namespace HT2000Viewer.Models
         {
             if (client == null) return;
             string strValue = value.ToString("F1");//  Convert.ToString("F1", value);
-            client.Publish("/home/temperature", Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            client.Publish(topic, Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
         }
 
 
@@ -70,7 +110,9 @@ namespace HT2000Viewer.Models
             //  Temperature = m.Temperature;
             //  Humidity = m.Humidity;
             //  CO2 = m.CO2;
-            Publish("/home/temperature", m.Temperature);
+            Publish(TemperatureTopic, m.Temperature);
+            Publish(HumidityTopic, m.Humidity);
+            Publish(CO2Topic, m.CO2);
         }
             // publish a message on "/home/temperature" topic with QoS 2 
 
